c913bd095a7b72ef71d0ecbe3d943efa081c3f71
wst07261144 <wst07261144@gmail.com>
2015-04-15 21:03:22 +0800

    add api for validate

--- app/api/twitter/api_v1.rb
+++ app/api/twitter/api_v1.rb
@@ -7,23 +7,53 @@ module Twitter
     format :json
     prefix :api
 
-
-    resource :users do
-      desc "Return all users."
-      get :get_all do
-        User.all
+    helpers do
+      def authenticate!
+        error!('Unauthorized. Invalid or expired token.', 401) unless current_user
       end
 
-      desc 'create a user'
+      def current_user
+        api_key = ApiKey.find_by(access_token: params[:token])
+        api_key && !api_key.expired? ? @current_user = User.find(api_key.user_id) : false
+      end
+    end
+
+    resource :auth do
+
+      desc "Creates and returns access_token if valid login"
       params do
-        requires :name, type: String, desc: "Your name."
-        requires :email, type: String, desc: "Your email."
-        requires :password, type: String, desc: "Your password."
+        requires :email, type: String, desc: "email"
+        requires :password, type: String, desc: "Password"
       end
-      post :create do
-        User.create(name: params[:name], email: params[:email], password: params[:password])
+      post :login do
+        user = User.find_by(email: params[:email].downcase, password: params[:password])
+        if user
+          user.api_key.blank? ? user.create_api_key : user.api_key.update_access_token
+          {access_token: user.api_key.access_token}
+        else
+          error!('Unauthorized.', 401)
+        end
       end
 
+      desc "update access_token if valid login"
+      params do
+        requires :email, type: String, desc: "email"
+        requires :password, type: String, desc: "Password"
+      end
+      post :update do
+        user = User.find_by(email: params[:email].downcase, password: params[:password])
+        user.api_key.update_access_token
+        {access_token: user.api_key.access_token}
+      end
+
+      desc "Returns pong if logged in correctly"
+      params do
+        requires :token, type: String, desc: "Access token."
+      end
+      get :ping do
+        authenticate!
+        { message: "pong" }
+      end
     end
 
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150414070806) do
+ActiveRecord::Schema.define(version: 20150415070701) do
+
+  create_table "api_keys", force: :cascade do |t|
+    t.string   "access_token"
+    t.datetime "expires_at"
+    t.integer  "user_id"
+    t.boolean  "active"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_index "api_keys", ["access_token"], name: "index_api_keys_on_access_token", unique: true
+  add_index "api_keys", ["user_id"], name: "index_api_keys_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
